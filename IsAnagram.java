be814c9fe281b7b4b9f23072dd1fd749b3d8ca52
robel <robelfekadu2017@gmail.com>
2019-12-02 16:13:54 -0800

    IsAnagramAdded

---
+++ IsAnagram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class IsAnagram{
+
+    public boolean isAnagram(String s, String t) {
+        char[] temp = s.toCharArray();
+        Arrays.sort(temp);
+        s = new String(temp);
+
+        temp = t.toCharArray();
+        Arrays.sort(temp);
+        t = new String(temp);
+
+        return s.equals(t);
+    }
+}

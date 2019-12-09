7b3a3e2b92a9a33e0f2e4e4ae75c733fed7c8406
robel <robelfekadu2017@gmail.com>
2019-12-09 15:16:38 -0800

    CodeForceContestQuestion2Added

---
+++ CodeForceContestQuestion2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class CodeForceContestQuestion2 {
+
+    public static void main(String[] args) {
+        BeautifulNumbers();
+    }
+
+    public static void  BeautifulNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        int[][] arrayOfNumbers = new int[testCases][];
+
+        for(int i = 0; i < testCases; i++){
+            int inputNumbersCount = scanner.nextInt();
+            int[] inputNumbersArray = new int[inputNumbersCount];
+            for (int j = 0; j < inputNumbersCount; j++) {
+                inputNumbersArray[j] = scanner.nextInt();
+            }
+
+            arrayOfNumbers[i] = inputNumbersArray;
+        }
+
+        for (int i = 0; i < testCases; i++){
+            System.out.println(CheckBeautifulNumber(arrayOfNumbers[i]));
+        }
+        scanner.close();
+    }
+
+    private static String CheckBeautifulNumber(int[] inputNumbers) {
+        int min, max;
+        int[] numbersIndex = new int[inputNumbers.length];
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < inputNumbers.length; i++) {
+            numbersIndex[inputNumbers[i] - 1] = i;
+        }
+
+        min = numbersIndex[0];
+        max = numbersIndex[0];
+
+        for (int i = 0; i < numbersIndex.length; i++) {
+            min = min > numbersIndex[i] ? numbersIndex[i] : min;
+            max = max < numbersIndex[i] ? numbersIndex[i] : max;
+
+            if (max - min == i)
+                result.append('1');
+            else 
+                result.append('0');
+        }
+
+        return result.toString();
+    }
+
+    private static int carFleet(int target, int[] position, int[] speed) {
+        HashMap<Integer, Integer> positionAndSpeedMap = new HashMap<>();
+        for (int i = 0; i < position.length; i++) {
+            positionAndSpeedMap.put(position[i], speed[i]);
+            positionAndSpeedMap.get(key)
+        }
+
+        for (int i = 0; i < position.length; i++) {
+            
+        }
+    }
+}
